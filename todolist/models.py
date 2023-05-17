d66879328ad36a982ad5bf58aadb9d6c715b09fb
Volney Casas <volneyrock@gmail.com>
2023-05-17 14:52:20 -0300

    create Base and Task models

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated at')
+
+    class Meta:
+        abstract = True
+
+
+class TaskModel(BaseModel):
+    title = models.CharField(max_length=255, verbose_name='Title', blank=True, null=True)
+    description = models.TextField(verbose_name='Description', blank=True, null=True)
+    completed = models.BooleanField(default=False, verbose_name='Completed')
+
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+
+    def __str__(self):
+        return self.title
