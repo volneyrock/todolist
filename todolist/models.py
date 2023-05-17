5a14861c343343f4814a0f44e1bd7a7212d01314
Volney Casas <volneyrock@gmail.com>
2023-05-17 17:42:47 -0300

    change pk in to uuid4

--- todolist/models.py
+++ todolist/models.py
@@ -1,7 +1,9 @@
+import uuid
 from django.db import models
 
 
 class BaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated at')
 
